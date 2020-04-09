7454bf4dca44afeebf28f990eba4602d3e529f0f
Ward obedi <obedixio@gmail.com>
2020-04-09 15:59:20 +0200

    Suoper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBox from './components/SearchBox';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search : "..." 
+    }
+  }
+ 
+  componentsDidMount = () => {
+    axios.get("/hashtags").then(Response => {
+      console.log(Response)
+    })
+  }; 
+  
   render() {
     return (
       <div className="App">
@@ -10,6 +24,8 @@ class App extends Component {
         <p className="App-intro">
           Try searching for a user name
         </p>
+    <button> Search {this.setState.search }</button>
+
         <SearchBox 
           placeholder="Search for .."
           items={['Eduard', 'god', 'Ward', 'Rahaf', 'Housni']} />
